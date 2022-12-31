99f128eaeacce1de4339aeb7134752804c932e68
LAH <00957047@email.ntou.edu.tw>
2023-01-01 01:53:42 +0800

    fix: swal font-size 過小問題

--- 2023.01.01/monopoly - 複製/php/ResponseStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-    final class ResponseStatusCode
-    {
-        static int $OK           = 200;
-        static int $LOGINFAILED  = 406;
-        static int $NAMETAKEN    = 409;
-        static int $ERROR        = 500;
-    }
-?>
